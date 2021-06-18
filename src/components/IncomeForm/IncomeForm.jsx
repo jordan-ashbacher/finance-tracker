30a7722232557aee3b822e63be1851a89fab0216
Jordan Ashbacher <jordan.ashbacher@daugherty.com>
2021-06-18 17:10:49 -0500

    creates submit handler, stores newIncome values

--- src/components/IncomeForm/IncomeForm.jsx
+++ src/components/IncomeForm/IncomeForm.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
-import InputBase from '@material-ui/core/InputBase'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+
+const useStyles = makeStyles({
+    formControl: {
+        minWidth: 100
+    }
+})
 
 const IncomeForm = () => {
 
     const history = useHistory()
+    const classes = useStyles()
 
     const [newIncome, setNewIncome] = useState({
         amount: '',
+        description: '',
         recurring: '',
     })
 
@@ -21,22 +32,47 @@ const IncomeForm = () => {
         console.log(newIncome)
     }
 
+    const submitIncomeForm = (e) => {
+        e.preventDefault()
+        console.log(newIncome)
+        setNewIncome({
+            amount: '',
+            description: '',
+            recurring: '',
+        })
+    }
+
     return(
         <div>
-            <h1>Add Income</h1>
-            <form>
-                <TextField label="Amount"/>
-                <TextField label="Description"/>
-                <Select 
-                    labelId="recurring-select=label"
-                    id="recurring-select"
-                    name="recurring"
-                    value={newIncome.recurring}
+            <h1>Add New Income</h1>
+            <form onSubmit={submitIncomeForm}>
+            
+                <TextField 
+                    label="Amount" 
+                    name="amount"
+                    value={newIncome.amount} 
                     onChange={handleFormChange}
-                >
-                    <MenuItem value={'Yes'}>Yes</MenuItem>
-                    <MenuItem value={'No'}>No</MenuItem>
-                </Select>
+                />
+                <TextField
+                    label="Description" 
+                    name="description" 
+                    value={newIncome.description}
+                    onChange={handleFormChange}
+                />
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="recurring-select=label">Recurring?</InputLabel>
+                        <Select 
+                            labelId="recurring-select=label"
+                            id="recurring-select"
+                            name="recurring"
+                            value={newIncome.recurring}
+                            onChange={handleFormChange}
+                        >
+                            <MenuItem value={'Yes'}>Yes</MenuItem>
+                            <MenuItem value={'No'}>No</MenuItem>
+                        </Select>
+                </FormControl>
+                <Button type="submit" variant="contained" color="primary">Add Income</Button>
             </form>
         </div>
 
